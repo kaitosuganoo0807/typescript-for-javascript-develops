44d3d64ea9a9ba2c8893c9a9fbba5bb0f538305c
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-03-25 22:03:45 +0900

    Type Aliases or typeof

---
+++ src/type-aliases.ts
@@ -0,0 +1,24 @@
+export {};
+
+type Mojiretsu = string;
+
+const fooString: string = 'Hello';
+const fooMojiretsu: Mojiretsu = 'Hello';
+
+const example1 = {
+  name: 'Ham',
+  age: 23
+};
+
+type Profile = {
+  name: string;
+  age: number;
+};
+
+const example2: Profile = {
+  name: 'Ham',
+  age: 23
+};
+
+type Profile2 = typeof example1;
+
