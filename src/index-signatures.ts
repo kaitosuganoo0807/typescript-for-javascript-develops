c0fa1a570757a521e5a96627d93c3ff82e1457d1
kaitosuganoo0807 <k.0807roadster@gmail.com>
2020-03-28 18:58:05 +0900

    index-signature

---
+++ src/index-signatures.ts
@@ -0,0 +1,16 @@
+export {};
+
+interface Profile {
+  name: string;
+  underTwenty: boolean;
+  [index: string]: string | number | boolean;
+}
+
+let profile: Profile = {name: 'kaito', underTwenty: false};
+
+// How to write index signatures
+// { [ index: typeForIndex ]: typeForValue }
+
+profile.name = 'kaito';
+profile.age = 23;
+profile.nationality = 'Japan';
